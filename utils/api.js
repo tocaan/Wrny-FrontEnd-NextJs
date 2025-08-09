fa20efafac3f3c67b9c3595fd1d215254969e2d9
Ahmed Naser <eng.ahmed.naserr@gmail.com>
2025-08-09 13:51:25 +0300

    Set Default Ar Language

--- utils/api.js
+++ utils/api.js
@@ -5,7 +5,7 @@ const api = axios.create({
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
-        'Accept-Language': (typeof window !== 'undefined' && localStorage.getItem('LANG')) || "en",
+        'Accept-Language': (typeof window !== 'undefined' && localStorage.getItem('LANG')) || "ar",
     },
 });
 
